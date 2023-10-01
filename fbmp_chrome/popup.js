df7a887482839482a2f3bd145447cab21ccbae3b
Payas Parab <pparab135@gmail.com>
2023-09-30 18:24:41 -0700

    appears to be bug in outlier removal stupid fucking log not working will revert back later

--- fbmp_chrome/popup.js
+++ fbmp_chrome/popup.js
@@ -8,10 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
             }, (injectionResults) => {
                 let results = injectionResults[0].result;
                 if (results && results.length) {
-                    let sum = results.reduce((a, b) => a + parseFloat(b.replace("$", "")), 0);
-                    let averagePrice = sum / results.length;
+                    console.log(results)
+                    let no_outs = removeOutliers(results)
+                    let no_out_data = no_outs.data
+                    let num_removed = no_outs.outliersRemoved
+                    let pct_out_removed = num_removed / results.length
+                    
+                    let sum = no_out_data.reduce((a, b) => a + parseFloat(b.replace("$", "")), 0);
+                    let averagePrice = sum / no_out_data.length;
+                    
                     document.getElementById("prices").textContent = "Average Price: $" + averagePrice.toFixed(2);
+                    document.getElementById("outliers_removed_count").textContent = "Removed " + 
+                        num_removed.toFixed(0) + " outliers. (" + formatAsPercentage(pct_out_removed) + ")";
                 } else {
+                    console.log("CONSOLE TEST")
                     document.getElementById("prices").textContent = "No prices found!";
                 }
             });
@@ -25,6 +35,42 @@ function fetchPricesFromHTML() {
     let matches = html.match(regex);
     return matches || [];
 }
+
+function removeOutliers(data) {
+    // Calculate the mean
+    let mean = data.reduce((acc, val) => acc + val, 0) / data.length;
+
+    // Calculate the standard deviation
+    let squaredDiffs = data.map(val => Math.pow(val - mean, 2));
+    let variance = squaredDiffs.reduce((acc, val) => acc + val, 0) / data.length;
+    let stdDev = Math.sqrt(variance);
+
+    // Filter out values that are outside 2 standard deviations from the mean
+    let filteredData = data.filter(val => Math.abs(val - mean) <= 2 * stdDev);
+
+    // Calculate the number of outliers removed
+    let outliersRemoved = data.length - filteredData.length;
+
+    console.log("Mean:", mean);
+    console.log("Standard Deviation:", stdDev);
+    console.log("Filtered data: ", filteredData);
+
+    return {
+        data: filteredData,
+        outliersRemoved: outliersRemoved
+    };
+}
+
+function formatAsPercentage(value) {
+    let formatter = new Intl.NumberFormat('en-US', {
+        style: 'percent',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    });
+
+    return formatter.format(value);
+}
+
 
 
 
